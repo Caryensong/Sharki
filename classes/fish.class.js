6c92be61e78119ccbdfbe2e18efb03444d5007fe
Caryen Song <caryensong@googlemail.com>
2025-01-20 11:57:13 +0100

    add emenies random position

--- classes/fish.class.js
+++ classes/fish.class.js
@@ -3,7 +3,11 @@ class Fish extends MovableObject{
     constructor(){
         super().loadImage('./assets/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
 
+        this.x = 200 + Math.random()* 500;
+        this.y = 0 + Math.random()* 400;
+
     }
+
     moveLeft(){
 
     }
